044cceaff71440729d4c061d71cb54ea401f00fc
crypticend <reece.devon@gmail.com>
2023-01-28 16:49:54 +1300

    Starting of filling out NPC sheet with data filting

--- module/sheets/npc-sheet.mjs
+++ module/sheets/npc-sheet.mjs
@@ -1,3 +1,6 @@
+import * as Dice from "../helpers/dice.mjs";
+import * as Helper from "../helpers/actor-helper.mjs";
+
 export class SBBNPCSheet extends ActorSheet{
 
     static get defaultOptions() {
@@ -14,6 +17,30 @@ export class SBBNPCSheet extends ActorSheet{
     getData(options) {
         const data = super.getData();
         data.config = CONFIG.SBB;
+
+        // Item filters
+        data.filteredItems = {
+            feats:  data.items.filter(function (item) {return item.type == "Feat"}),
+            attacks:  data.items.filter(function (item) {return item.type == "Weapon"}),
+            consumables:  data.items.filter(function (item) {return item.type == "Consumable"}),
+            armour:  data.items.filter(function (item) {return item.type == "Armour"}),
+            otherItems:  data.items.filter(function (item) {return item.type == "Item"}),
+
+            enhancementDrug: data.items.filter(function (item) {return item.type == "Effect"
+                && item.system.type == "SBB.effects.drug"}),
+            enhancementImplant: data.items.filter(function (item) {return item.type == "Effect"
+                && item.system.type == "SBB.effects.implant"}),
+            injury: data.items.filter(function (item) {return item.type == "Effect"
+                && item.system.type == "SBB.effects.injury"})
+        }
+
+        let skills = data.items.filter(function (item) {return item.type == "Skill"});
+        data.filteredItems.skills = {};
+
+        for (const [key,value] of Object.entries(data.config.skillTypes)){
+            data.filteredItems.skills[key] = skills.filter(function (item) {return item.system.attribute==key});
+        }
+
 
         return data;
     }
