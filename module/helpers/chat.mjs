1e4a1f8a9a4750c2a35c98755bf8e73ffa1ba7f7
crypticend <reece.devon@gmail.com>
2023-01-28 21:28:08 +1300

    Got actor tokens rolling weapon correctly

--- module/helpers/chat.mjs
+++ module/helpers/chat.mjs
@@ -24,14 +24,63 @@ export function addChatListeners(html){
 
 function onAttack(event){
     const card = event.currentTarget.parentNode;
+
+    // Only for "token" attackers
+    let tokenId = card.dataset.tokenId;
+    if(tokenId != null){
+        onTokenAttack(tokenId, card.dataset.itemId);
+        return;
+    }
+
     let attacker = game.actors.get(card.dataset.ownerId);
     let weapon = attacker.items.get(card.dataset.itemId);
+
+    rollAttackGivenAttacker(attacker, weapon);
+}
+
+function onTokenAttack(tokenId, weaponId){
+    // Token ID includes both a scene and a token ID
+    let tokenSplit = tokenId.split("-");
+    let sceneId;
+    tokenId = null;
+
+    // Search the array for the key values we need
+    for (const [key, value] of Object.entries(tokenSplit)) {
+
+
+        if(value.toLowerCase() == "scene"){
+            sceneId = tokenSplit[parseInt(key)+1];
+        }
+        else if(value.toLowerCase() == "token"){
+            tokenId = tokenSplit[parseInt(key)+1];
+        }
+    }
+
+    // Check we have the values we need
+    if(tokenId == null || sceneId == null){
+        console.error(game.i18n.localize("SBB.errors.cannotFindForWeaponRoll"));
+        return;
+    }
+
+    let scene = game.scenes.get(sceneId);
+    let tokenActor = scene.tokens.get(tokenId);
+
+    let weapon = tokenActor._actor.items.get(weaponId);
+
+    rollAttackGivenAttacker(tokenActor._actor, weapon);
+}
+
+function rollAttackGivenAttacker(attacker, weapon){
+    if(attacker==null || weapon == null){
+        console.error(game.i18n.localize("SBB.errors.weaponOrAttackerUndefined"));
+        return;
+    }
 
     // make sure attacker has relevant skill
     const linkedSkill= weapon.system.skill;
     let skill = attacker.items.filter(function (skill) {
         return skill.type === "Skill" &&
-                skill.name.toUpperCase() === game.i18n.localize(linkedSkill).toUpperCase()});
+            skill.name.toUpperCase() === game.i18n.localize(linkedSkill).toUpperCase()});
 
     if(skill.length === 0) {
         console.error(game.i18n.localize("SBB.errors.unfoundedSkill") + linkedSkill.toUpperCase());
